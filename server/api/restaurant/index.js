bf259a56d3dfbe3afb25d7ebef4c720523ae76eb
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-29 01:19:16 +0530

    Restaurant api revised

--- server/api/restaurant/index.js
+++ server/api/restaurant/index.js
@@ -10,8 +10,7 @@ const Router = express.Router();
  * Access    Public
  * Method    POST
  */
-// Home work
-Router.post("/",async (req,res)=>{
+Router.post("/new",async (req,res)=>{
     try {
         const {restaurantData} = req.body;
         const restaurant = await RestaurantModel.create({...restaurantData})
@@ -23,7 +22,7 @@ Router.post("/",async (req,res)=>{
 
 /**
  * Route     /
- * Des       Get all restaurant details based on city
+ * Des     Get all the restuarant details based on the city
  * Params    none
  * Access    Public
  * Method    GET
@@ -33,8 +32,8 @@ Router.get("/",async (req,res)=>{
         // http://localhost:4000/restaurant/?city=delhi
         const {city} = req.query;
         const restaurants = await RestaurantModel.find({city});
-        if(!restaurants){
-            return res.status(404).json({error: "Resturant not found"})
+        if(restaurants.length === 0){
+            return res.status(404).json({error: "No restaurant found in this city."})
         }
         return res.json({restaurants})
     } catch (error) {
@@ -51,7 +50,7 @@ Router.get("/",async (req,res)=>{
 Router.get("/:_id",async (req,res)=>{
     try {
         const {_id} = req.params;
-        const restaurant = await RestaurantModel.findOne(_id);
+        const restaurant = await RestaurantModel.findById(_id);
         if(!restaurant){
             return res.status(404).json({error:"Resturant not found"})
         }
@@ -62,27 +61,30 @@ Router.get("/:_id",async (req,res)=>{
 })
 /**
  * Route     /search/:searchString
- * Des       Get restaurant details based on search string
- * Params    _id
+ * Des       Get restaurants details based on search string
+ * Params    searchString
  * Access    Public
  * Method    GET
  */
 Router.get("/search/:searchString",async (req,res)=>{
-    /**
-     * Search String = Raj
-     * result = {
-     * Raj Hotel,
-     * Raj Palace,
-     * Raja Hotel,}
-     */
+   /**
+   * searchString = Raj
+   * results = {
+   *  RajHotel
+   *  RajRow
+   *  RonRaj
+   *  raJRow
+   * }
+   */
     try {
         const {searchString} = req.params;
         const restaurants = await RestaurantModel.find({
             name: {$regex:searchString, $options:"i"}
         })
-        if(!restaurants){
+        if(!restaurants.length === 0){
             return res.status(404).json({error:`No Resturant matched with ${searchString}`})
         }
+        return res.json({restaurants});
     } catch (error) {
         return res.status(500).json({error: error.message})
     }
