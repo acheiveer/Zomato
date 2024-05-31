5b768078446374171ba6f3b6aaebf916d364cce0
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-06-01 00:44:33 +0530

    craeted files and folder for compoennt page and layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
+import {Routes,Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello this is app.js</h1>
-    </div>
+   <>
+    <Routes>
+
+    </Routes>
+   </>
   );
 }
 
