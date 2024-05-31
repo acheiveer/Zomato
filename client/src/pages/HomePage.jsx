d8df589cccc7fd6a5fd689a112668a4299106305
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-06-01 03:41:03 +0530

    Delivery Carousel and Restaurant Card Completed

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 //Layout
 import HomePageLayout from "../layouts/HomePageLayout";
 
+//Component
+import Delivery from "../components/Delivery/Delivery";
+import Dining from "../components/Dining/Dining";
+import NightLife from "../components/NightLife/NightLife";
+import Nutrition from "../components/Nutrition/Nutrition";
 
-const HomePage = () =>{
+const HomePage = () => {
+    const { type } = useParams;
     return (
         <>
-            <div>Homepage</div>
+            <div className="my-5 mb-20 md:mb-10">
+                {type === "delivery" && <Delivery />}
+                {type === "dining" && <Dining />}
+                {type === "night" && <NightLife />}
+                {type === "nutrition" && <Nutrition />}
+            </div>
         </>
     )
 }
